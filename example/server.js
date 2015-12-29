031bb06f13178e1ca8b5f17e4c2a9ac4d0645968
bloodyKnuckles <treenopie@gmail.com>
2015-12-28 19:02:00 -0600

    refactoring

--- example/server.js
+++ example/server.js
@@ -1,64 +1,35 @@
 var cookieHawk = require('../')
-var hawk = require('hawk')
 var http = require('http') // USE HTTPS FOR PRODUCTION!
 
 var fs = require('fs')
 var body = require('body/any')
-var cookieObject = require('cookie-object')
 
 function validateUser (user) {
-  if ( !!('j' === user.uname && '4' === user.pword ) ) {
-    return Object.keys(cookieHawk.getCredentials()).reduce(function (prev, next) {
-      return user.uname === cookieHawk.getCredentials(next).user? next: prev
-    }, '')
-  }
-  return undefined
-}
-
-function credentialsFunc (id, callback) {
-  return callback(null, cookieHawk.getCredentials(id))
+  if ( !!('j' === user.uname && '4' === user.pword ) ) { return true }
+  return false
 }
 
 // HTTP server handler
 var handler = function (req, res) {
   var requrlarr = req.url.split('?')
-  if ( 'POST' === req.method && '/login' === requrlarr[0] && undefined === requrlarr[1] ) {
+  if ( 'POST' === req.method ) {
     body(req, res, function (err, postvars) {
-      var id
-      if ( id = validateUser({uname:postvars.uname, pword:postvars.pword}) ) {
-        cookieHawk.clientSendCredentials(res, {id:id})
+      if ( '/login' === requrlarr[0] ) {
+        if ( validateUser({uname:postvars.uname, pword:postvars.pword}) ) {
+          cookieHawk.clientSendCredentials(res, postvars.uname)
+        }
+        else {
+          responseStream('/login.html')
+        }
       }
       else {
-        responseStream('/login.html')
+        cookieHawk.auth(req, res, function () {
+        })
       }
     })
   }
   else {
-    var reqheaders = req.headers.authorization ||
-      cookieObject.getCookieObject(req.headers.cookie, 'hawkauth') ||
-      {}
-
-    hawk.server.authenticate(reqheaders, credentialsFunc, {}, function (err, credentials, artifacts) {
-      var redirectto = cookieObject.getCookieObject(req.headers.cookie, 'redirectto')
-
-      if ( err && 'string' === typeof redirectto && 0 === redirectto.indexOf('/login') ) {
-        res.setHeader('Set-Cookie', 'redirectto=; expires=' + Date(0) + ';')
-        responseStream('/login.html')
-      }
-      else if ( err ) { // && artifacts ) {
-        res.setHeader('Set-Cookie', 'redirectto=/login;')
-        cookieHawk.clientSendCredentials(res)
-      }
-      else {
-        res.setHeader('Set-Cookie', 'redirectto=; expires=Thu, 01 Jan 1970 00:00:00 GMT;')
-        var payload = (!err ? 'Hello ' + credentials.user + ' ' + (artifacts.ext || '') : 'Shoosh!')
-        var headers = {
-          'Content-Type': 'text/plain',
-          'Server-Authorization': hawk.server.header(credentials, artifacts, { payload: payload, contentType: 'text/plain' })
-        }
-        res.writeHead(!err ? 200 : 401, headers)
-        res.end(payload)
-      }
+    cookieHawk.auth(req, res, function () {
     })
   }
 
@@ -67,6 +38,7 @@ var handler = function (req, res) {
     res.writeHead(200, {'Content-Type': type }) 
     fs.createReadStream(__dirname + '/public' + resource).pipe(res)
   }
+
 }
 
 http.createServer(handler).listen(8000, 'localhost')
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var fs = require('fs')
 
+var hawk = require('hawk')
+var cookieObject = require('cookie-object')
+
 var internals = {
   credentials: {
     dh37fgj492je: {
@@ -11,23 +14,75 @@ var internals = {
   }
 }
 
-module.exports = {
+function credentialsFunc (id, callback) {
+  return callback(null, getCredentials(id))
+}
 
-  getCredentials: function (id) {
-    return internals.credentials[id] || internals.credentials
+function getCredentials (id) {
+  return internals.credentials[id] || internals.credentials
+}
+
+var cookieHawk = {
+
+  auth: function (req, res, callback) {
+    var reqheaders = req.headers.authorization ||
+      cookieObject.getCookieObject(req.headers.cookie, 'hawkauth') ||
+      {}
+
+    hawk.server.authenticate(reqheaders, credentialsFunc, {}, function (err, credentials, artifacts) {
+      var redirectto = cookieObject.getCookieObject(req.headers.cookie, 'redirectto')
+
+      if ( err && 'string' === typeof redirectto && 0 === redirectto.indexOf('/login') ) {
+        deleteCookie(res, 'redirecto')
+        responseStream('/login.html')
+      }
+      else if ( err ) { // && artifacts ) {
+        res.setHeader('Set-Cookie', 'redirectto=/login;')
+        cookieHawk.clientSendAuth(res)
+      }
+      else {
+        deleteCookie(res, 'redirecto')
+        var payload = 'Hello ' + credentials.user + ' ' + (artifacts.ext || '')
+        var headers = {
+          'Content-Type': 'text/plain',
+          'Server-Authorization': hawk.server.header(credentials, artifacts, { payload: payload, contentType: 'text/plain' })
+        }
+        res.writeHead(200, headers)
+        res.end(payload)
+      }
+    })
   },
 
-  clientSendCredentials: function (res, setcredentials) {
-    var credentials = setcredentials && getCredentials(setcredentials.id)
+  clientSendCredentials: function (res, username) {
+    var id = Object.keys(cookieHawk.getCredentials()).reduce(function (prev, next) {
+      return username === cookieHawk.getCredentials(next).user? next: prev
+    }, '')
+    var credentials = id && getCredentials(id)
     var setcredentialsstr = credentials
       ? '<script>localStorage.setItem("credentials", \'' +
         JSON.stringify(credentials) +
-        '\')</script>': ''
+        '\')</script>': undefined
+    cookieHawk.clientSendAuth(res, id, setcredentialsstr)
+  },
+
+  clientSendAuth: function (res, setcredentialsstr) {
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(setcredentialsstr + '<script>')
+    res.write((setcredentialsstr || '') + '<script>')
     res.write(fs.readFileSync('./lib/browser-min.js'))
     res.write('</script>')
     res.end()
   }
 
+}
+
+module.exports = cookieHawk
+
+function responseStream (res, resource, type) {
+  type = type || 'text/html'
+  res.writeHead(200, {'Content-Type': type })
+  fs.createReadStream(__dirname + '/public' + resource).pipe(res)
+}
+
+function deleteCookie (res, name) {
+  res.setHeader('Set-Cookie', name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT;')
 }
